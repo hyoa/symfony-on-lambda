a3d3136cffe240942c420f04e3a2b34b13b03d21
Jules Matsounga <jmatsounga.externe@m6.fr>
2019-11-07 11:30:20 +0100

    chore: first commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/config/bootstrap.php';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$_SERVER['SCRIPT_NAME'] = '/dev/index.php';
+if (strpos($_SERVER['REQUEST_URI'], '/dev') === false) {
+    $_SERVER['REQUEST_URI'] = '/dev'.$_SERVER['REQUEST_URI'];
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
